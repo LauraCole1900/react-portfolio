29b40053cd480dbef61cffd23e0c9394d05db059
Laura Cole <lauracole1900@comcast.net>
2020-11-27 12:14:20 -0700

    added projects.json and screenshot files

--- my-portfolio/src/components/navbar/index.js
+++ my-portfolio/src/components/navbar/index.js
@@ -1,79 +1,35 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Link, useLocation } from "react-router-dom";
+import Nav from "react-bootstrap/Nav";
 
 // https://codepen.io/BrunoIF/pen/oNjNepa - transparent React navbar
 
-// const { useEffect, useState } = React
-
 const Navigation = () => {
-  const [offsetTop, setOffsetTop] = useState(0)
   const location = useLocation();
-  let thisElement = React.createRef()
-
-  useEffect(() => {
-    window.addEventListener('scroll', () => setOffsetTop(window.scrollY))
-  }, [])
-
-  useEffect(() => {
-
-    // var tl = new TimelineMax()
-
-    if (offsetTop === 0) {
-
-      const style = {
-        backgroundColor: 'transparent',
-        borderBottom: '1px solid rgba(255, 255, 255, .1)',
-        boxShadow: 'none',
-        color: '#F0F0F2',
-      }
-
-      // tl
-      //   .to(thisElement, .3, style, 'a')
-      //   .to('.navbar__container', .3, {padding: '20px 70px', ease: Power1.easeOut}, 'a')
-    }
-    else {
-
-      const style = {
-        backgroundColor: '#F0F0F2',
-        borderBottom: '0',
-        boxShadow: '0px 2px 10px 0px rgba(0,0,0,.3)',
-        color: '#222126',
-      }
-
-      // tl
-      //   .to(thisElement, .3, style, 'a')
-      //   .to('.navbar__container', .3, {padding: '20px 55px'}, 'a')
-    }
-  }, [offsetTop])
 
   return (
-    <div className="navbar" ref={el => thisElement = el}>
-      <div className="navbar__container">
-        <div className="navbar__logo">LOGO</div>
-        <ul className="navbar__items">
-          <li className="navbar__link">
-            <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
-              Home
-            </Link>
-          </li>
-          <li class="navbar__link">
-            <Link to="/about" className={location.pathname === "/about" ? "nav-link active" : "nav-link"}>
-              About
-            </Link>
-          </li>
-          <li class="navbar__link">
-            <Link to="/portfolio" className={location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}>
-              Portfolio
-            </Link>
-          </li>
-          <li class="navbar__link">
-            <Link to="/contact" className={location.pathname === "/contact" ? "nav-link active" : "nav-link"}>
-              Contact Me
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </div>
+    <Nav defaultActiveKey="/" className="navbar">
+      <Nav.Item>
+        <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
+          Home
+        </Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Link to="/about" className={location.pathname === "/about" ? "nav-link active" : "nav-link"}>
+          About
+        </Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Link to="/portfolio" className={location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}>
+          Portfolio
+        </Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Link to="/contact" className={location.pathname === "/contact" ? "nav-link active" : "nav-link"}>
+          Contact Me
+        </Link>
+      </Nav.Item>
+    </Nav >
   )
 }
 
--- my-portfolio/src/components/pages/about.js
+++ my-portfolio/src/components/pages/about.js
@@ -4,9 +4,11 @@ function About() {
   return (
     <div>
       <h1>About Me</h1>
-      <p>
-        About Me
-      </p>
+      <p class="biotext">I grew up in LaSalle, a small town in northeastern Colorado. After graduating from the <a href="http://www.unco.edu">University of Northern Colorado</a> Lab School in nearby Greeley, I went on to earn my Bachelor of Music Education from the university proper. I then went to work at Milliken Elementary School as the music teacher and stayed there for several years before deciding to pursue web development.</p>
+
+      <p class="biotext">In September of 2020, I started the University of Denver Coding Bootcamp program to certify as a full-stack web developer. I have always enjoyed both helping people and creative endeavors, and web development seems a good way to combine those passions.</p>
+
+      <p class="biotext">In my spare time, I still enjoy making music with the <a href="http://www.greeleychorale.org">Greeley Chorale</a> and training in martial arts. I train in <a href="https://www.skski.net/">Kosho Ryu Kempo</a>, <a href="http://www.koryu-uchinadi.com">Koryu Uchinadi</a>, and Shotokan.</p>
     </div>
   );
 }
