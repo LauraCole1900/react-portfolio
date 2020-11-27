fd6cc1f2a8c4536e8360690e9431c6080b51478a
Laura Cole <lauracole1900@comcast.net>
2020-11-27 13:31:09 -0700

    animated landing page does not work yet

--- my-portfolio/src/components/hero/index.js
+++ my-portfolio/src/components/hero/index.js
@@ -1,15 +1,32 @@
 import React, { Component } from "react";
-import Longspic from "../../assets/images/meeker-n-longs.jpg"
+// import Longspic from "../../assets/images/meeker-n-longs.jpg"
 
 // https://codepen.io/ljf4/pen/xVBYgL - animated text over hero
 
 class Hero extends Component {
+  state = {
+    text: ""
+  }
+
+  Hero() {
+    this.text.typed({
+      strings: ["WEB DEVELOPER", "MUSICIAN", "MARTIAL ARTIST", "CAT LOVER"],
+      typeSpeed: 50,
+      backSpeed: 20,
+      backDelay: 2000,
+      showCursor: true,
+      loop: true
+    });
+  };
+
   render() {
     return (
-      <>
-        <img src={Longspic} />
-      </>
-  )
+      <section className="hero">
+        <div className="hero-text">
+          <p className="text"></p>
+        </div>
+      </section>
+    )
   }
 }
 
