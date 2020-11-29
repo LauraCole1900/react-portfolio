90e7895fa4a5f2f476d2b38ec9b47f37f5989530
Laura Cole <lauracole1900@comcast.net>
2020-11-28 19:55:14 -0700

    button styling

--- my-portfolio/src/components/pages/portfolio.js
+++ my-portfolio/src/components/pages/portfolio.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Row, Col } from "react-bootstrap";
 import ProjectCard from "../project";
 import projects from "../../projects.json";
 import "./style.css"
@@ -9,19 +10,29 @@ class Portfolio extends Component {
   render() {
     return (
       <>
-        <h1>Portfolio</h1>
-        {this.state.projects.map(project => (
-          <ProjectCard
-            id={project.id}
-            key={project.id}
-            name={project.name}
-            image={project.image}
-            description={project.description}
-            repo={project.repo}
-            deployed={project.deployed}
-          />
-        ))}
-      </>
+        <Row>
+          <Col sm={1}></Col>
+          <Col sm={10}>
+            <h1>Portfolio</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={1}></Col>
+          <Col sm={10}>
+            {this.state.projects.map(project => (
+              <ProjectCard
+                id={project.id}
+                key={project.id}
+                name={project.name}
+                image={project.image}
+                description={project.description}
+                repo={project.repo}
+                deployed={project.deployed}
+              />
+            ))}
+          </Col>
+        </Row>
+        </>
     );
   }
 }
