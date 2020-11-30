44c932b3253952f81c940ac966aa401b14dce60b
Laura Cole <lauracole1900@comcast.net>
2020-11-30 12:33:20 -0700

    added rel= code on links

--- my-portfolio/src/components/footer/index.js
+++ my-portfolio/src/components/footer/index.js
@@ -13,10 +13,10 @@ const Footer = () => {
       <Row>
         <Col sm={10}></Col>
         <Col sm={1}>
-          <a href="https://www.linkedin.com/in/laura-cole-3661b01b9/?trk=profile-badge" target="_blank"><Image fluid src={linkedin} alt="LinkedIn icon" className="icon" /></a>
+          <a href="https://www.linkedin.com/in/laura-cole-3661b01b9/?trk=profile-badge" rel="noreferrer noopener" target="_blank"><Image fluid src={linkedin} alt="LinkedIn icon" className="icon" /></a>
         </Col>
         <Col sm={1}>
-          <a href="https://github.com/LauraCole1900" target="_blank"><Image fluid src={githublt} alt="GitHub icon" className="icon" /></a>
+          <a href="https://github.com/LauraCole1900" rel="noreferrer noopener" target="_blank"><Image fluid src={githublt} alt="GitHub icon" className="icon" /></a>
         </Col>
       </Row>
     </div >
--- my-portfolio/src/components/pages/contact.js
+++ my-portfolio/src/components/pages/contact.js
@@ -34,7 +34,7 @@ function Contact() {
         <Col sm={1}></Col>
         <Col sm={2}>
           <p className="git">GitHub</p>
-          <a href="https://github.com/LauraCole1900" target="_blank"><Image fluid src={github} alt="GitHub icon" className="icon" /></a>
+          <a href="https://github.com/LauraCole1900" rel="noreferrer noopener" target="_blank"><Image fluid src={github} alt="GitHub icon" className="icon" /></a>
         </Col>
         <Col sm={6}>
           <p>email: <a href="mailto:lauracole1900@comcast.net">lauracole1900@comcast.net</a></p>
@@ -45,7 +45,7 @@ function Contact() {
         <Col sm={1}></Col>
         <Col sm={2}>
           <p className="git">Résumé</p>
-          <a href="https://drive.google.com/file/d/1gOy3CkufWH_k_Y9dVFQYfZc955bPCT25/view?usp=sharing" target="_blank"><Image fluid src={resume} alt="Résumé icon" className="icon" /></a>
+          <a href="https://drive.google.com/file/d/1gOy3CkufWH_k_Y9dVFQYfZc955bPCT25/view?usp=sharing" rel="noreferrer noopener" target="_blank"><Image fluid src={resume} alt="Résumé icon" className="icon" /></a>
         </Col>
       </Row>
     </Container >
