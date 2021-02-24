995d7768fe010e213ad2b908c34f28ff2b8700d7
Laura Cole <lauracole1900@comcast.net>
2021-02-23 20:30:06 -0700

    added technical skills section to webdev page

--- my-portfolio/src/components/pages/webdev.js
+++ my-portfolio/src/components/pages/webdev.js
@@ -3,6 +3,20 @@ import { Row, Col, Image } from "react-bootstrap";
 import ProjectCard from "../project";
 import projects from "../../projects.json";
 import github from "../../pix/GitHub-Mark-120px-plus.png";
+import htmlicon from "../../pix/html5-icon.png";
+import cssicon from "../../pix/css3-icon.png";
+import bootstrapicon from "../../pix/bootstrap-icon.png";
+import materializeicon from "../../pix/materialize-icon.png"
+import jsicon from "../../pix/js-icon.png";
+import jqueryicon from "../../pix/jquery-icon.png";
+import nodejsicon from "../../pix/nodejs-icon.png";
+import npmicon from "../../pix/npm-icon.png";
+import expressjsicon from "../../pix/expressjs-icon.png";
+import mysqlicon from "../../pix/mysql-icon.png";
+import mongodbicon from "../../pix/mongodb-icon.png";
+import reacticon from "../../pix/react-icon.png";
+import mernicon from "../../pix/MERN-icon.png";
+import githubicon from "../../pix/GitHub-Mark-120px-plus.png"
 import "./style.css"
 
 class WebPortfolio extends Component {
@@ -46,6 +60,36 @@ class WebPortfolio extends Component {
               />
             ))}
           </Col>
+        </Row>
+        <Row className="topMargin">
+          <Col sm={1}></Col>
+          <Col sm={10}><h1>Technical Skills</h1></Col>
+        </Row>
+        <Row>
+          <Col sm={1}></Col>
+          <Col sm={10}>
+            <Image fluid src={htmlicon} alt="HTML5" className="iconBig" />
+            <Image fluid src={cssicon} alt="CSS3" className="iconBig" />
+            <Image fluid src={bootstrapicon} alt="Bootstrap" className="iconBig darkerBg" />
+            <Image fluid src={materializeicon} alt="Materialize" className="iconBig" />
+            <Image fluid src={jsicon} alt="JavaScript" className="iconBig" />
+            <Image fluid src={jqueryicon} alt="jQuery" className="iconBig" />
+            <Image fluid src={nodejsicon} alt="Node.js" className="iconBig darkBg" />
+            <Image fluid src={npmicon} alt="NPM" className="iconBig" />
+            <Image fluid src={expressjsicon} alt="Express" className="iconBig darkerBg" />
+            <Image fluid src={mysqlicon} alt="MySQL" className="iconBig darkBg" />
+            <Image fluid src={mongodbicon} alt="MongoDB" className="iconBig darkBg" />
+            <Image fluid src={githubicon} alt="GitHub" className="iconBig" />
+            <Image fluid src={reacticon} alt="React" className="iconBig" />
+            <Image fluid src={mernicon} alt="MERN" className="iconBig" />
+          </Col>
+          <Col sm={1}></Col>
+        </Row>
+        <Row>
+          <Col sm={1}></Col>
+          <Col sm={10}>
+          </Col>
+          <Col sm={1}></Col>
         </Row>
       </>
     );
